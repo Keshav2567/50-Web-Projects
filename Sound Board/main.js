9c0fd410024596b90b42424ab1df848932d3645c
Keshav2567 <70498256+Keshav2567@users.noreply.github.com>
2021-07-01 12:34:44 +0530

    Add files via upload

---
+++ Sound Board/main.js
@@ -0,0 +1,23 @@
+let sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+
+sounds.forEach(sound => {
+	let btn = document.createElement('button');
+	btn.classList.add('btn');
+
+	btn.innerText = sound;
+
+	btn.addEventListener('click', () => {
+		stopSound();
+		document.getElementById(sound).play();
+	});
+	
+	document.getElementById('buttons').appendChild(btn);
+});
+
+function stopSound() {
+	sounds.forEach(sound => {
+		const audio = document.getElementById(sound);
+		audio.pause();
+		audio.currentTime = 0;
+	});
+}
